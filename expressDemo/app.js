078907b77f2f373a86ad8912f30da3af0ecc72cd
Michel-Yang <18826220374@163.com>
2019-05-25 22:41:05 +0800

    新增评论

--- expressDemo/app.js
+++ expressDemo/app.js
@@ -1,14 +1,72 @@
-const express = require('express')
-const app = express()
+var express = require("express");
+//引入POST请求包
+var bodyParser = require("body-parser")
 
-app.use('/public', express.static('public'))
 
+var app = express();
+
+//配置静态资源
+app.use('/public/', express.static('./public/'))
+
+//配置模板引擎
 app.engine('html', require('express-art-template'));
 
-app.get('/', (req, res) => res.send('Hello World! hello node.js'))
+//配置post请求
 
-app.get('/express', (req, res) => res.render('index.html',{
-    text:"你好，世界"
+app.use(bodyParser.urlencoded({
+    extended: false
 }))
+app.use(bodyParser.json())
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var comments = [{
+        name: "小李",
+        message: "今天天气很好",
+        dateTime: "2019-5-25"
+    },
+    {
+        name: "小李",
+        message: "今天天气很好",
+        dateTime: "2019-5-25"
+    },
+    {
+        name: "小李",
+        message: "今天天气很好",
+        dateTime: "2019-5-25"
+    }
+]
+
+app.get('/', function (req, res) {
+    res.render('index.html', {
+        comments: comments
+    })
+})
+
+
+
+/* app.get('/pinlun', function (req, res) {
+    var comment = req.query;
+    comment.dateTime = "2019-5-25 21:52";
+    comments.unshift(comment);
+    res.redirect('/')
+}) */
+
+
+app.get('/post', function (req, res) {
+    res.render('post.html')
+})
+
+//添加评论
+
+app.post('/post', function (req, res) {
+    var comment = req.body;
+    comment.dateTime = "2019-5-25 21:52";
+    comments.unshift(comment);
+    res.redirect('/')
+
+})
+
+
+
+app.listen(3000, function () {
+    console.log("express is running...")
+})
